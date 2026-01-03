ae827ce1713b85cd370637d369015387ae3ab815
Jeffrey McGovern <jeff@jdmcg.org>
2026-01-03 14:48:51 -0800

    read an FRM file
    https://github.com/zing22845/go-frm-parser?tab=readme-ov-file#parse-from-file

---
+++ frm-go/read.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	frm "github.com/zing22845/go-frm-parser/frm"
+	"os"
+)
+
+func main() {
+	path := os.Args[1]
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
+
+	// read and parse frm file
+	result, err := frm.Parse(path, file)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Printf("====WITHOUT HEADER:\n%s", result.String())
+	fmt.Printf("\n====WITH HEADER:\n%s", result.StringWithHeader())
+}
