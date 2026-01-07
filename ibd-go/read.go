b691685ca2218dcc7e7776c90222436618626cb4
Jeffrey McGovern <jeff@jdmcg.org>
2026-01-06 20:59:10 -0800

    doesn't work: tablespace does not have SDI

---
+++ ibd-go/read.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tidwall/pretty"
+	ibd2schema "github.com/zing22845/go-ibd2schema"
+	"os"
+)
+
+func main() {
+	filePath := os.Args[1]
+	fmt.Println("Opening:", filePath)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("NewTableSpace:", file)
+	ts, err := ibd2schema.NewTableSpace(file)
+	if err != nil {
+		panic(err)
+	}
+
+	// dump ddl: only support file per table = On
+	fmt.Println("DumpSchemas:", ts)
+	err = ts.DumpSchemas()
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(-1)
+	}
+
+	for db, table := range ts.TableSchemas {
+		fmt.Printf("Database: %s\n", db)
+		fmt.Printf("Table DDL: %s\n", table.DDL)
+	}
+	// dump sdi
+	err = ts.DumpSDIs()
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(-1)
+	}
+	fmt.Println(string(pretty.Pretty(ts.SDIResult)))
+}
